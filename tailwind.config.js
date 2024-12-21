fdeace37c05d6d410c24abbbf89c60b5d2dcf54f
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-12-22 01:46:04 +0530

    tailwind and folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        background: "var(--color-background)",
+        text: "var(--color-text)",
+        "text-secondary": "var(--color-text-secondary)",
+        "button-hover": "var(--color-button-hover)",
+      },
+    },
+  },
+  plugins: [],
+};
